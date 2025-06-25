da3e2d0e68a92c6dbc896308ca998d710d950864
Antonio Fernández <afm@mss.mx>
2025-06-24 19:10:34 -0600

    probando git flow primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+// app.module.ts
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
